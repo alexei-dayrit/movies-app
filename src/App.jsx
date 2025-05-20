bb403c3f772e209ce5fce8415e12dbe4424a93a1
Alexei Dayrit <alexeigdayrit@gmail.com>
2025-05-20 03:57:08 -0700

    Added curly brackets to conditional rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import "./css/App.css";
 import MovieCard from "./components/MovieCard";
 
 function App() {
+  const movieNumber = 1;
+
   return (
     <>
-      <MovieCard movie={{ title: "Alex's Film", release_date: "2024" }} />
-      <MovieCard movie={{ title: "Christelle's Film", release_date: "2020" }} />
+      {movieNumber === 1 ? (
+        <MovieCard movie={{ title: "Alex's Film", release_date: "2024" }} />
+      ) : (
+        <MovieCard
+          movie={{ title: "Christelle's Film", release_date: "2020" }}
+        />
+      )}
     </>
   );
 }
