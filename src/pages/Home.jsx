aa172e36b6b34f7b13cd6ff5e966f14270c0fdd7
Alexei Dayrit <alexeigdayrit@gmail.com>
2025-05-20 14:45:52 -0700

    Added format for search and added useState.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard";
+import { usestate } from "react";
 
 const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Terminator", release_date: "199" },
@@ -17,6 +20,9 @@ const Home = () => {
           type="text"
           placeholder="Search for movies..."
         />
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
       <div className="movies-grid">
         {movies.map((movie) => (
