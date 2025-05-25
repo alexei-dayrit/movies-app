5b91782b4525c0b86c18c07f43ac7804b4d935c5
Alexei Dayrit <alexeigdayrit@gmail.com>
2025-05-25 06:26:18 -0700

    Added proper image display.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,11 @@
 import "../css/MovieCard.css";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 const MovieCard = ({ movie }) => {
+  const imageUrl = movie.poster_path
+    ? `${IMAGE_BASE_URL}${movie.poster_path}`
+    : null;
+
   const onFavoriteClick = () => {
     alert("clicked!");
   };
@@ -8,7 +13,7 @@ const MovieCard = ({ movie }) => {
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title} />
+        <img src={imageUrl} alt={movie.title} />
         <button className="favorite-btn" onClick={onFavoriteClick}>
           ♥
         </button>
