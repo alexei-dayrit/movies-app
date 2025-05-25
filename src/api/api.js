107ba2c8958e182ff72978062e20116d8c7554a8
Alexei Dayrit <alexeigdayrit@gmail.com>
2025-05-25 05:43:12 -0700

    Created getPopularMovies and searchMovies functions.

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,20 @@
 console.log("ENV:", import.meta.env.API_KEY);
 
-export const getPopularMovies = async () => {};
+const API_KEY = import.meta.env.API_KEY;
+const BASE_URL = import.meta.env.BASE_URL;
+
+export const getPopularMovies = async () => {
+  const repsonse = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const repsonse = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
